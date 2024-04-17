354f83fa66c656574d800d0de207dc4e7c037749
Tito Fariñas <titofarinas@outlook.com>
2024-04-17 17:19:43 -0600

    Adding styles with tailwind

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,22 +1,44 @@
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => {
+
+const activeStyle = 'underline underline-offset-4'
+
   return (
-    <nav>
-        <ul>
-            <li><NavLink to="/">Shopi</NavLink></li>
-            <li><NavLink to="/">All</NavLink></li>
-            <li><NavLink to="/clothes">Clothes</NavLink></li>
-            <li><NavLink to="/Electronics">Electronics</NavLink></li>
-            <li><NavLink to="/Furnitures">Furnitures</NavLink></li>
-            <li><NavLink to="/toys">Toys</NavLink></li>
+    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
+        <ul className='flex items-center gap-3'>
+         
+            <li className='font-semibold text-lg'><NavLink to="/"           
+            >Shopi</NavLink></li>
+            <li><NavLink to="/"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>All</NavLink></li>
+            <li><NavLink to="/clothes"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>Clothes</NavLink></li>
+            <li><NavLink to="/Electronics"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>Electronics</NavLink></li>
+            <li><NavLink to="/Furnitures"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>Furnitures</NavLink></li>
+            <li><NavLink to="/toys"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>Toys</NavLink></li>
         </ul>
 
-        <ul>
-            <li>titofarinas@outlook.com</li>
-            <li><NavLink to="/my-orders">My Orders</NavLink></li>
-            <li><NavLink to="/my-account">My Account</NavLink></li>
-            <li><NavLink to="/sign-in">Sign In</NavLink></li>
+        <ul className='flex items-center gap-3'>
+            <li className='text-black/60'>titofarinas@outlook.com</li>
+            <li><NavLink to="/my-orders"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>My Orders</NavLink></li>
+            <li><NavLink to="/my-account"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>My Account</NavLink></li>
+            <li><NavLink to="/sign-in"  className={({ isActive }) => 
+            isActive ? activeStyle : undefined
+          }>Sign In</NavLink></li>
             <li>🛒 9</li>            
         </ul>
     </nav>
